087a8157b3215709e27780d891acbdd3ba4b6e71
AngelTravieso <angeltraviesoc@gmail.com>
2025-01-20 00:51:48 -0400

    Pantalla de permiso de GPS

--- lib/screens/gps_access_screen.dart
+++ lib/screens/gps_access_screen.dart
@@ -7,7 +7,50 @@ class GpsAccessScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('GPS Access Screen'),
+        child: _AccessButton(),
+      ),
+    );
+  }
+}
+
+class _AccessButton extends StatelessWidget {
+  const _AccessButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Es necesario el acceso a GPS'),
+        FilledButton(
+          style: FilledButton.styleFrom(
+            backgroundColor: Colors.black,
+          ),
+          onPressed: () {
+            // TODO: por hacer
+          },
+          child: Text(
+            'Solicitar Acceso',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _EnableGpsMessage extends StatelessWidget {
+  const _EnableGpsMessage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Debe habilitar el GPS',
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w300,
       ),
     );
   }
